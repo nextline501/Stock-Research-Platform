24418580ddffe2671b8972d73a032e34e1b3c965
Jacob <Jacob.SH@icloud.com>
2021-03-15 10:46:55 +0100

    commit 1

--- app/index.js
+++ app/index.js
@@ -1,19 +1,8 @@
 const app = require('express')()
-import simpleData from "./simpleCoords4326.json"
-import geojsonData from "./geojsonCoords4326.json"
-import geojsonTest from "./Geotest2_4326.geojson"
-var path = require('path');
 
-app.get('/api/simpleCoords4326', (req, res) =>{
-  res.send(JSON.stringify(simpleData))
+app.get('/api/test', (req, res) =>{
+  res.send("yo")
 })
 
-app.get('/api/geojsonCoords4326', (req, res) =>{
-  res.send(JSON.stringify(geojsonData))
-})
-
-app.get('/api/geojsonTest4326', (req, res) =>{
-  res.sendFile(path.join(__dirname, '../app', 'Geotest2_4326.geojson'));
-})
 
 module.exports = app
